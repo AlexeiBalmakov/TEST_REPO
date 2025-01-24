cd661cd6ff1be174633475b531758e73e8f7138f
AlexeiBalmakov <balmakov.alexei@yandex.ru>
2025-01-24 14:48:26 +0300

    тест пуш ветка 2

--- src/main/java/org/example/testfx/HelloApplication.java
+++ src/main/java/org/example/testfx/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("пуш на сервер");
+        System.out.println("Ветка 2 тест");
     }
 
     public static void main(String[] args) {
