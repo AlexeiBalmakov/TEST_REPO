2ecf4e3056a7465071f0e78813df30e1aa1b54d9
AlexeiBalmakov <balmakov.alexei@yandex.ru>
2025-01-24 14:44:00 +0300

    тест

---
+++ src/main/java/org/example/testfx/HelloApplication.java
@@ -0,0 +1,24 @@
+package org.example.testfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Калькулятор");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
